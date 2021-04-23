3dc7fdd66bd904f00db682d133235e49b6b5c249
eugene <eugenecarbado@gmail.com>
2021-04-23 22:26:04 +0100

    .

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,12 @@
-document.getElementById('hamburger').addEventListener('click', showNav);
-document.getElementById('product').addEventListener('click', showSubMenu);
-document.getElementById('company').addEventListener('click', showSubMenu);
-document.getElementById('connect').addEventListener('click', showSubMenu);
-window.addEventListener('resize', removeToggle);
+const menus = document.getElementsByClassName("hamburger_menu");
 
-const navMenu = document.getElementById('nav_bar');
+[].forEach.call(menus, function (m) {
+    m.addEventListener('click', function() {
+        const links = document.querySelector(".nav_links");
+        m.classList.toggle('open');
+        links.classList.toggle('nav_links_open');
+    });
+});
 
-function showNav() {
-    navMenu.classList.toggle('burger_active');
-}
 
-function removeToggle() {
-    if (window.innerWidth > 900) {
-        navMenu.classList.remove('burger_active');
-    }
-}
-
-function showSubMenu(e) {
-    e.preventDefault;
-   if ( window.innerWidth < 900) {
-        e.target.nextElementSibling.classList.toggle('sub_item_active');
-    e.target.firstElementChild.classList.toggle('rotate');
-   }
-}
-
-export { showNav, showSubMenu }
+// export { }
