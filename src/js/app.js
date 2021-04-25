d74925e441de11871caec53b2dca6a9293338c2c
eugene <eugenecarbado@gmail.com>
2021-04-25 13:32:01 +0100

    .

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,7 @@ const menus = document.getElementsByClassName("hamburger_menu");
     m.addEventListener('click', function() {
         const links = document.querySelector(".nav_links");
         m.classList.toggle('open');
+        // links.style.transform = "translateX(0%)";
         links.classList.toggle('nav_links_open');
     });
 });
