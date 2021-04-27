4334603ef28c45e8ab412418583cc59c4e5a6156
eugene <eugenecarbado@gmail.com>
2021-04-27 22:57:46 +0100

    .

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,10 @@ import "../css/header.scss";
 import "../css/reset.scss";
 import "../css/colours.scss";
 import "../css/about-me.scss";
+import "../css/skills.scss";
 import "../css/portfolio.scss";
+import "../css/contact.scss";
+import "../css/footer.scss";
 
 import { showNav } from "../js/app";
 import { showSubMenu } from "../js/app";
